91c7af622efa90d4f52c9cc0c14c92b454a6f6d0
chibatuz <chibatuz@gmail.com>
2020-12-01 13:45:12 +0400

    New commit_02

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int a = 35;
+	// the cost of gasoline
+        double price = 42.50;
+        int volume = 50;
+        double money = price * volume;
+
+        System.out.println(money);
+
+    // the net pay
+        int gross_pay = 70000;
+        int tax = 13;
+        double pay_tax = gross_pay / 100 * tax;
+        double net_pay = gross_pay - pay_tax;
+
+        System.out.println(net_pay);
+
+    // the envelope
+        double seconds = 3600;
+        int minute_per_hour = 60;
+        double hour = seconds / minute_per_hour;
+
+        System.out.println(hour + " min");
+
+
     }
 }
